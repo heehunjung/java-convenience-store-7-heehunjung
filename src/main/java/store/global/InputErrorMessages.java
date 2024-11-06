7fd172ae8321ca3c41777d4b15caf126256ee18d
heehunjung <slrkqk2@naver.com>
2024-11-06 18:19:55 +0900

    feat(InputConstant,InputErrorMessages): 상수와 에러메세지 관리하는 클래스, 이넘 생성

---
+++ src/main/java/store/global/InputErrorMessages.java
@@ -0,0 +1,20 @@
+package store.global;
+
+public enum InputErrorMessages {
+    INVALID_INPUT_PATTERN("올바르지 않은 형식으로 입력했습니다."),
+    INVALID_INPUT_NULL_EMPTY("빈 값을 입력하시면 안됩니다."),
+    INVALID_YES_OR_NO_INPUT("Y 또는 N만 입력할 수 있습니다.");
+
+
+    private String message;
+    private String code;
+
+    private InputErrorMessages(String message) {
+        this.message = message;
+        this.code = "[ERROR] ";
+    }
+
+    public String getMessage() {
+        return code + message;
+    }
+}
