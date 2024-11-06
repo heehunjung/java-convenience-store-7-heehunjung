9db6b87b116d41f0ffd3136ce609a44d72006e25
heehunjung <slrkqk2@naver.com>
2024-11-06 21:29:28 +0900

    test(ProductTest): 상품 수량 체크 메서드 테스트 작성

--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -9,15 +9,33 @@ public class ProductTest {
 
     @DisplayName("updateStock_테스트_01")
     @Test
-    void 기능_테스트() {
+    void updateStock_기능_테스트() {
         String name = "치킨";
         int price = 1000;
         int stock = 10;
 
-        Product product = new Product(name,price,stock,null);
-        product.update(10);
+        Product product = new Product(name, price, stock, null);
+        product.updateStock(10);
 
         Assertions.assertThat(product.getStock())
                 .isEqualTo(0);
+    }
+
+    @DisplayName("isStockAvailable_테스트_01")
+    @Test
+    void isStockAvailable_기능_테스트() {
+        int inValidInput = 11;
+        int validInput = 10;
+
+        String name = "치킨";
+        int price = 1000;
+        int stock = 10;
+
+        Product product = new Product(name, price, stock, null);
+
+        Assertions.assertThatThrownBy(() -> product.isStockAvailable(inValidInput))
+                .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatNoException()
+                .isThrownBy(() ->  product.isStockAvailable(validInput));
     }
 }
