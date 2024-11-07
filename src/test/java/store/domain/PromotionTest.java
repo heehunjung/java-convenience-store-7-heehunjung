c24641bfd758691a86252121cbb09ad120812c46
heehun <slrkqk2@naver.com>
2024-11-07 19:15:09 +0900

    test(PromotionTest): 프로모션 수량을 계산하는 메서드 테스트 작성

--- src/test/java/store/domain/PromotionTest.java
+++ src/test/java/store/domain/PromotionTest.java
@@ -10,24 +10,33 @@ import org.junit.jupiter.api.Test;
 
 public class PromotionTest {
 
-    @DisplayName("checkDate_메서드_테스트_01")
+    @DisplayName("checkDate_테스트_01")
     @Test
     void checkData_기능_테스트() {
-        LocalDate startTime = LocalDate.of(2024,10,1);
-        LocalDate endTime_valid = LocalDate.of(2024,12,30);
-
-        String promotionName = "2+1";
-        BuyGet three_but_one_get = new BuyGet(3,1);
-        Range validRange = new Range(startTime,endTime_valid);
-
-        Promotion promotion = new Promotion(promotionName,three_but_one_get,validRange);
+        Promotion promotion = getTestPromotion();
         Assertions.assertThat(promotion.checkDate(now()))
                 .isTrue();
     }
 
-    @DisplayName("getBuyCount_메서드_테스트_01")
+    @DisplayName("getTotalBuyStock_테스트_01")
     @Test
-    void getBuyStock_기능_테스트() {
+    void getTotalBuyStock_기능_테스트() {
+        Promotion promotion = getTestPromotion();
+
+        Assertions.assertThat(promotion.getTotalBuyStock(10))
+                .isEqualTo(6);
+    }
+
+    @DisplayName("calculateFreeStock_테스트_01")
+    @Test
+    void calculateFreeStock_기능_테스트() {
+        Promotion promotion = getTestPromotion();
+
+        Assertions.assertThat(promotion.calculateFreeStock(12))
+                .isEqualTo(4);
+    }
+
+    private Promotion getTestPromotion() {
         LocalDate startTime = LocalDate.of(2024,10,1);
         LocalDate endTime_valid = LocalDate.of(2024,12,30);
 
@@ -35,9 +44,6 @@ public class PromotionTest {
         BuyGet three_but_one_get = new BuyGet(3,1);
         Range validRange = new Range(startTime,endTime_valid);
 
-        Promotion promotion = new Promotion(promotionName,three_but_one_get,validRange);
-
-        Assertions.assertThat(promotion.getBuyStock(10))
-                .isEqualTo(6);
+        return new Promotion(promotionName,three_but_one_get,validRange);
     }
 }
