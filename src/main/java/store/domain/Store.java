2b377a49499fee34d2ee42464fabae2812e1b6e3
heehun <slrkqk2@naver.com>
2024-11-07 21:37:25 +0900

    feat(Store): 전체 동작 로직 작성
    메서드 분리 예정

--- src/main/java/store/domain/Store.java
+++ src/main/java/store/domain/Store.java
@@ -1,6 +1,13 @@
 package store.domain;
 
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Store {
     private final List<Product> products;
@@ -9,6 +16,77 @@ public class Store {
     public Store(List<Product> products, List<Promotion> promotions) {
         this.products = products;
         this.promotions = promotions;
+    }
+
+    public void run(Map<String,Integer> shoppingCarts) {
+        shoppingCarts.forEach((product,stock)->{
+            Product nomalProduct = findProduct(product);
+            Product promotionalProduct = findPromotionProduct(product);
+
+            //메서드 1 ====================================================
+            if (nomalProduct ==  null && promotionalProduct == null) {
+                throw new IllegalStateException("잘못된 입력임 다시 입력하셈");
+            }
+            //============================================================
+
+            //메서드 2 ====================================================
+            int promotionStock = 0 ;
+            int normalStock = 0 ;
+            if (promotionalProduct != null) {
+                promotionStock = promotionalProduct.getStock();
+            }
+            if (nomalProduct != null) {
+                normalStock = nomalProduct.getStock();
+            }
+
+            if (promotionStock+normalStock < stock) {
+                throw new IllegalStateException("수량   src/main/java/store/domain/Receipt.java\n버그");
+            }
+            // ==========================================================
+
+            List<Product> purchasedProducts = new ArrayList<Product>();
+
+            // 메서드3 ====================================================
+            int purchasePromotionalProductNumber = 0 ;
+            int noPromotionalProductNumber = 0 ;
+            int notFreePromotionalProductNumber = 0 ;
+            int freeProductNumber = 0 ;
+            int promotionalProductNumber = 0 ;
+            if (promotionalProduct != null ) {
+                if (promotionalProduct.getPromotion().checkDate(DateTimes.now())) {
+                    // TODO : 여기 프로모션 구매 검증 들어가야함
+                    purchasePromotionalProductNumber = promotionalProduct.getPromotion().getTotalBuyStock(stock);
+                    freeProductNumber = promotionalProduct.getPromotion().calculateFreeStock(purchasePromotionalProductNumber);
+
+                    if (stock > 0 && promotionalProduct.getStock() > 0 ) {
+                        noPromotionalProductNumber = promotionalProduct.getStock();
+                    }
+
+                    notFreePromotionalProductNumber = noPromotionalProductNumber + purchasePromotionalProductNumber;
+                    promotionalProductNumber = notFreePromotionalProductNumber + freeProductNumber;
+
+                    promotionalProduct.updateStock(promotionalProductNumber);
+
+                    Product purchasedProduct = new Product(promotionalProduct,notFreePromotionalProductNumber, FALSE);
+                    Product freeProduct = new Product(promotionalProduct,freeProductNumber, TRUE);
+                    purchasedProducts.add(purchasedProduct);
+                    purchasedProducts.add(freeProduct);
+                }
+            }
+            // ===================================================================
+
+            int remainStock = stock - promotionalProductNumber;
+
+            // 메서드 4 ===========================================================
+            if (remainStock > 0) {
+                // TODO : NULL  인 경우 ? 내 생각엔 없는데,
+                nomalProduct.updateStock(remainStock);
+                Product purchasedProduct = new Product(nomalProduct,remainStock, FALSE);
+                purchasedProducts.add(purchasedProduct);
+            }
+            // ===================================================================
+        });
+
     }
 
     public Product findProduct(String input) {
@@ -27,5 +105,13 @@ public class Store {
             }
         }
         return null;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public List<Promotion> getPromotions() {
+        return promotions;
     }
 }
