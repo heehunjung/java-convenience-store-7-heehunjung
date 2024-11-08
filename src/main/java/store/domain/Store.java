6fbf32b1091a519e0d8f2baa80f2d633be3d6e57
heehunjung <slrkqk2@naver.com>
2024-11-09 01:15:33 +0900

    feat(store): store 클래스 구현

--- src/main/java/store/domain/Store.java
+++ src/main/java/store/domain/Store.java
@@ -1,87 +1,20 @@
 package store.domain;
 
 import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
 import static store.global.ErrorMessages.INVALID_INPUT_STOCK;
 import static store.global.ErrorMessages.PRODUCT_NOT_FOUND;
 
-import camp.nextstep.edu.missionutils.DateTimes;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import store.domain.promotion.Promotion;
 
 public class Store {
-    //TODO : Product -> Item
 
-    // 필드 변수
     private final List<Item> items;
     private final List<Promotion> promotions;
 
-    // 생성자
     public Store(List<Item> items, List<Promotion> promotions) {
         this.items = items;
         this.promotions = promotions;
-    }
-
-    // 공용 메서드
-    public void run(Map<String, Stock> shoppingCarts) {
-        shoppingCarts.forEach((product, stock) -> {
-            Item nomalItem = findProduct(product);
-            Item promotionalItem = findPromotionProduct(product);
-
-            // 상품 존재 여부 확인
-            isProductExists(nomalItem, promotionalItem);
-
-            // 유효한 수량 확인
-            isValidStock(stock, promotionalItem, nomalItem);
-
-            List<Item> purchasedItems = new ArrayList<Item>();
-
-            // 프로모션 제품 처리
-            processPromotionalProducts(stock, promotionalItem, purchasedItems);
-
-            // 일반 제품 처리
-            processNormalProducts(stock, nomalItem, purchasedItems);
-        });
-    }
-
-    //TODO : INDENT + 10즐
-    public void processPromotionalProducts(Stock stock, Item promotionalItem, List<Item> purchasedItems) {
-        Stock stockFreeItem = new Stock(0);
-        Stock stockForPay = new Stock(0);
-
-        if (promotionalItem == null) {
-            return;
-        }
-        if (promotionalItem.checkDate(DateTimes.now())) {
-            if (buyPromoItemNoDiscount(stock, promotionalItem, purchasedItems)) {
-                return;
-            }
-
-            promotionProcess(stock, promotionalItem, stockFreeItem, stockForPay);
-
-            addPurchaseProduct(stockForPay, promotionalItem, purchasedItems, FALSE);
-            addPurchaseProduct(stockFreeItem, promotionalItem, purchasedItems, TRUE);
-        }
-    }
-
-    private static void promotionProcess(Stock stock, Item promotionalItem, Stock stockFreeItem, Stock stockForPay) {
-        int stockForPromotionItem = promotionalItem.getTotalBuyStock(stock.getStock());
-        int remainingStock = processRemainingPromotionStock(promotionalItem);
-
-        stockFreeItem.plus(promotionalItem.calculateFreeStock(stockForPromotionItem));
-        stockForPay.plus(remainingStock + stockForPromotionItem);
-        int total = stockForPay.getStock() + stockFreeItem.getStock();
-        promotionalItem.updateStock(total);
-        stock.minus(total);
-    }
-
-    public void processNormalProducts(Stock remainStock, Item nomalItem, List<Item> purchasedItems) {
-        if (remainStock.getStock() > 0) {
-            nomalItem.updateStock(remainStock.getStock());
-            addPurchaseProduct(remainStock, nomalItem, purchasedItems, FALSE);
-        }
     }
 
     public boolean buyPromoItemNoDiscount(Stock stock, Item promotionalItem, List<Item> purchasedItems) {
@@ -97,6 +30,7 @@ public class Store {
         return false;
     }
 
+    // TODO: 다른 곳으로 빼야 될듯 STATIC 아니면 ITEM 쪽으로
     public void isProductExists(Item nomalItem, Item promotionalItem) {
         if (nomalItem == null && promotionalItem == null) {
             throw new IllegalArgumentException(PRODUCT_NOT_FOUND.getMessage());
@@ -138,13 +72,7 @@ public class Store {
         return promotions;
     }
 
-    // private 메서드
-    private static int processRemainingPromotionStock(Item promotionalItem) {
-        return Math.max(promotionalItem.getStock()
-                .getStock(), 0);
-    }
-
-    private void addPurchaseProduct(Stock stock, Item nomalItem, List<Item> purchasedItems, Boolean isFree) {
+    public static void addPurchaseProduct(Stock stock, Item nomalItem, List<Item> purchasedItems, Boolean isFree) {
         Item purchasedItem = new Item(nomalItem, stock, isFree);
         purchasedItems.add(purchasedItem);
     }
