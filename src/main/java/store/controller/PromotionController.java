e16eb1bf79dcbe87ae6a5465a91b68f3e9c31598
heehun <slrkqk2@naver.com>
2024-11-10 15:09:31 +0900

    feat(PromotionController): 파일의 값들을 바탕으로 promotion 객체 리스트를 생성하는 메서드 작성

---
+++ src/main/java/store/controller/PromotionController.java
@@ -0,0 +1,36 @@
+package store.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import store.domain.promotion.BuyGet;
+import store.domain.promotion.Promotion;
+import store.domain.promotion.Range;
+import store.utils.Converter;
+import store.utils.Parser;
+
+public class PromotionController {
+
+    public List<Promotion> setPromotions(List<String> information) {
+        List<Promotion> promotions = new ArrayList<Promotion>();
+        for (String promotionString : information.subList(1, information.size())) {
+            List<String> fields = Parser.splitWithCommaDelimiter(promotionString);
+            promotions.add(setPromotion(fields));
+        }
+
+        return promotions;
+    }
+
+    private Promotion setPromotion(List<String> input) {
+        String name = input.get(0);
+        String buy = input.get(1);
+        String get = input.get(2);
+        String start = input.get(3);
+        String end = input.get(4);
+
+        BuyGet buyGet = new BuyGet(Integer.parseInt(buy), Integer.parseInt(get));
+        Range range = new Range(Converter.stringToLocalDate(start), Converter.stringToLocalDate(end));
+
+        return new Promotion(name, buyGet, range);
+    }
+
+}
