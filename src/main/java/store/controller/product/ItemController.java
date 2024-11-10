dee03698be85d4dca8da1d94ccf1a5d04d0f1181
heehun <slrkqk2@naver.com>
2024-11-10 16:05:19 +0900

    feat(ItemController): setItems 메서드 구현

--- src/main/java/store/controller/product/ItemController.java
+++ src/main/java/store/controller/product/ItemController.java
@@ -3,9 +3,12 @@ package store.controller.product;
 import static java.lang.Boolean.FALSE;
 import static store.domain.Store.addPurchaseProduct;
 
+import java.util.ArrayList;
 import java.util.List;
 import store.domain.Item;
 import store.domain.Stock;
+import store.domain.promotion.Promotion;
+import store.utils.Parser;
 
 public class ItemController {
 
@@ -16,4 +19,35 @@ public class ItemController {
             addPurchaseProduct(remainStock, nomalItem, purchasedItems, FALSE);
         }
     }
-}
+
+    public List<Item> setItems(List<String> information,List<Promotion> promotions) {
+        List<Item> items = new ArrayList<Item>();
+
+        for (String item : information.subList(1, information.size())) {
+            List<String> fields = Parser.splitWithCommaDelimiter(item);
+            items.add(setItem(fields, promotions));
+        }
+
+        return items;
+    }
+
+    private Item setItem(List<String> information,List<Promotion> promotions) {
+        String name = information.get(0);
+        String price = information.get(1);
+        String quantity = information.get(2);
+        String promotionName = information.get(3);
+
+        Stock stock = new Stock(Parser.stringToInt(quantity));
+        Promotion promotion = findPromotionByName(promotionName, promotions);
+
+        return new Item(name, Integer.parseInt(price), stock, promotion);
+    }
+
+    private Promotion findPromotionByName(String name,List<Promotion> promotions) {
+        for (Promotion promotion : promotions) {
+            if (promotion.getName().equals(name)) {
+                return promotion;
+            }
+        }
+        return null;
+    }}
