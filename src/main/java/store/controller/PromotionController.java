d7eb03fcb8b65a0c0b1b774e6add012a1048c3a3
heehun <slrkqk2@naver.com>
2024-11-10 16:06:39 +0900

    refactor(PromotionController): 읽어온 첫 라인 제외

--- src/main/java/store/controller/PromotionController.java
+++ src/main/java/store/controller/PromotionController.java
@@ -12,6 +12,7 @@ public class PromotionController {
 
     public List<Promotion> setPromotions(List<String> information) {
         List<Promotion> promotions = new ArrayList<Promotion>();
+
         for (String promotionString : information.subList(1, information.size())) {
             List<String> fields = Parser.splitWithCommaDelimiter(promotionString);
             promotions.add(setPromotion(fields));
@@ -20,6 +21,7 @@ public class PromotionController {
         return promotions;
     }
 
+
     private Promotion setPromotion(List<String> input) {
         String name = input.get(0);
         String buy = input.get(1);
@@ -27,7 +29,7 @@ public class PromotionController {
         String start = input.get(3);
         String end = input.get(4);
 
-        BuyGet buyGet = new BuyGet(Integer.parseInt(buy), Integer.parseInt(get));
+        BuyGet buyGet = new BuyGet(Parser.stringToInt(buy), Parser.stringToInt(get));
         Range range = new Range(Converter.stringToLocalDate(start), Converter.stringToLocalDate(end));
 
         return new Promotion(name, buyGet, range);
